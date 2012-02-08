8d3e65c1148dd89914a8827438d47e1fc4f207c5
dotiendiep@gmail.com <dotiendiep@gmail.com@62067d32-7b27-d987-aaab-571dba77ff5c>
2012-02-08 18:20:10 +0000

    Update nội dung full tiếng việt cho tệp CheckLangBehavior.class.php

--- trunk/Extend/Behavior/CheckLangBehavior.class.php
+++ trunk/Extend/Behavior/CheckLangBehavior.class.php
@@ -2,28 +2,28 @@
 
 /**
  +------------------------------------------------------------------------------
- * Thao tác mở rộng hệ thống 语言检测 并自动加载语言包
+ * Thao tác mở rộng hệ thống Phát hiện ngôn ngữ và tự tải gói ngôn ngữ
  +------------------------------------------------------------------------------
  */
 class CheckLangBehavior extends Behavior {
-    // Tham số định nghĩa cho thao tác mở rộng（默认值） 可在项目配置中覆盖
+    // Tham số định nghĩa cho thao tác mở rộng (mặc định) có thể định nghĩa lại trong các project
     protected $options   =  array(
-            'LANG_SWITCH_ON'        => false,   // 默认关闭语言包功能
-            'LANG_AUTO_DETECT'      => true,   // 自动侦测语言 开启多语言功能后有效
-            'LANG_LIST' => 'zh-cn', // 允许切换的语言列表 用逗号分隔
-            'VAR_LANGUAGE'          => 'l',		// 默认语言切换变量
+            'LANG_SWITCH_ON'        => false,   // Thiết lập chuyển đổi ngôn ngữ
+            'LANG_AUTO_DETECT'      => true,   // Tự động phát hiện ngôn ngữ
+            'LANG_LIST' => 'vi-vn,en-us', // Danh sách ngôn ngữ hỗ trợ
+            'VAR_LANGUAGE'          => 'l',		// Biến chuyển đổi ngôn ngữ
         );
 
     // Thao tác mở rộng cần phải được run
     public function run(&$params){
-        // 开启静态缓存
+        // Check ngôn ngữ
         $this->checkLanguage();
     }
 
     /**
      +----------------------------------------------------------
-     * 语言检查
-     * 检查浏览器支持语言，并自动加载语言包
+     * Kiểm tra ngôn ngữ
+     * Kiểm tra xem trình duyệt đang sử dụng ngôn ngữ nào, tự động tải gói ngôn ngữ thích hợp
      +----------------------------------------------------------
      * @access private
      +----------------------------------------------------------
@@ -31,41 +31,41 @@ class CheckLangBehavior extends Behavior {
      +----------------------------------------------------------
      */
     private function checkLanguage() {
-        // 不开启语言包功能，仅仅加载框架语言文件直接返回
+        // 
         if (!C('LANG_SWITCH_ON')){
             return;
         }
         $langSet = C('DEFAULT_LANG');
-        // 启用了语言包功能
-        // 根据是否启用自动侦测设置获取语言选择
+        // Mở chức năng đa ngôn ngữ
+        // Kiểm tra xem có bật chức năng tự động phát hiện hay không, để lấy thông số cho ngôn ngữ đã chọn
         if (C('LANG_AUTO_DETECT')){
             if(isset($_GET[C('VAR_LANGUAGE')])){
-                $langSet = $_GET[C('VAR_LANGUAGE')];// url中设置了语言变量
+                $langSet = $_GET[C('VAR_LANGUAGE')];// Lấy tham số ngôn ngữ từ url
                 cookie('think_language',$langSet,3600);
-            }elseif(cookie('think_language')){// 获取上次用户的选择
+            }elseif(cookie('think_language')){// Lấy thông số mà người dùng lựa chọn cuối
                 $langSet = cookie('think_language');
-            }elseif(isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])){// 自动侦测浏览器语言
+            }elseif(isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])){// Tự động phát hiện ngôn ngữ trình duyệt sử dụng
                 preg_match('/^([a-z\-]+)/i', $_SERVER['HTTP_ACCEPT_LANGUAGE'], $matches);
                 $langSet = $matches[1];
                 cookie('think_language',$langSet,3600);
             }
-            if(false === stripos(C('LANG_LIST'),$langSet)) { // 非法语言参数
+            if(false === stripos(C('LANG_LIST'),$langSet)) { // Tham số ngôn ngữ không hợp lệ
                 $langSet = C('DEFAULT_LANG');
             }
         }
-        // 定义当前语言
+        // Định nghĩa ngôn ngữ hiện hành
         define('LANG_SET',strtolower($langSet));
-        // 读取项目公共语言包
+        // Đọc tệp ngôn ngữ dùng chung cho gói ngôn ngữ lựa chọn
         if (is_file(LANG_PATH.LANG_SET.'/common.php'))
             L(include LANG_PATH.LANG_SET.'/common.php');
         $group = '';
-        // 读取当前分组公共语言包
+        // Đọc gói ngôn ngữ dành cho nhóm project hiện hành
         if (defined('GROUP_NAME')){
             $group = GROUP_NAME.C('TMPL_FILE_DEPR');
             if (is_file(LANG_PATH.LANG_SET.'/'.$group.'lang.php'))
                 L(include LANG_PATH.LANG_SET.'/'.$group.'lang.php');
         }
-        // 读取当前模块语言包
+        // Đọc gói ngôn ngữ dành cho module hiện hành
         if (is_file(LANG_PATH.LANG_SET.'/'.$group.strtolower(MODULE_NAME).'.php'))
             L(include LANG_PATH.LANG_SET.'/'.$group.strtolower(MODULE_NAME).'.php');
     }
