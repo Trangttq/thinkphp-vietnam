3a88cc9a1d0632fc04320e644492f9050f28849b
dotiendiep <dotiendiep@gmail.com>
2012-06-13 05:22:04 +0000

    Change for new project (all based on ThinkPHP)

---
+++ App/Conf/config.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+    'APP_AUTOLOAD_PATH' => '@ORG.Uitl',
+    'URL_MODEL' => 2,
+    'APP_GROUP_LIST' => 'Home,Admin',
+    'DEFAULT_GROUP' => 'Home',
+    'DB_FIELDTYPE_CHECK' => true,
+    'TMPL_STRIP_SPACE' => true,
+    'DEFAULT_THEME' => 'default',
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'thinkphp',
+    'DB_USER' => 'root',
+    'DB_PWD' => '',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'think_',
+    'VAR_PAGE' => 'p',
+);
+?>
